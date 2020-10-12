2c6a4d7d86e68fcb4da6ee5dfc1e6bf6c4fb81b6
Cyprian  Nyakundi <cn@Cyprians-MacBook-Pro.local>
2020-10-12 08:18:03 +0300

    more code on input

--- run.py
+++ run.py
@@ -58,3 +58,39 @@ def display_account():
     #function to display all accounts saved
 
     return Credentials.display_accounts()
+
+
+
+def main():
+
+    print("\nHello and welcome!")
+    print("\n 1. Create an account \n 2. Exit app")
+    option = input()
+
+    if option == '1':
+        print("Enter your name")
+        account_owner = input()
+        if not account_owner:
+            print("You must type something in")
+        else:
+            print("Enter a password")
+            account_key = input()
+            if not account_key:
+                print("You must type something in")
+            else:
+                save_user(create_user(account_owner, account_key))
+                print("-" * 10)
+                print(f"{account_owner}, please enter your password again to login.")
+                confirm = input()
+                if confirm == account_key:
+                    print("\n You are successfully logged in!")
+                    while True:
+                        print("-" * 10)
+                        print(
+                            "What would you like to do today? \n Use these short codes : \n \t\t aa - Add an account "
+                            "using your own password \n \t\t ga - Generate a password for your new account \n \t\t da "
+                            "- display saved accounts \n \t\t sa - Search for an account \n \t\t D - delete an "
+                            " account \n \t\t ex - Exit")
+                        short_code = input().lower()
+
+                        

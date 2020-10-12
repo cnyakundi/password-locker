cd9bb7f04fa530cbb7f6eb2987c4d10305b8ef0d
Cyprian  Nyakundi <cn@Cyprians-MacBook-Pro.local>
2020-10-12 08:19:11 +0300

    more functions

--- run.py
+++ run.py
@@ -93,4 +93,72 @@ def main():
                             " account \n \t\t ex - Exit")
                         short_code = input().lower()
 
-                        
+                        if short_code == 'aa':
+                            print("-" * 10)
+                            print("Enter an existing account")
+                            print("Account name ....(eg: facebook)")
+                            account = input()
+                            print("Username ...")
+                            u_name = input()
+                            print("Password ...")
+                            passwd = input()
+                            save_accounts(create_account(account, u_name, passwd))
+                            print('\n')
+                            print(f"New account: {account} added \n Username: {u_name} --- Password:{passwd}")
+                            print('\n')
+
+                        elif short_code == 'ga':
+                            print("-" * 10)
+                            print("Account name ....(eg: facebook)")
+                            account = input()
+                            print("Username ...")
+                            u_name = input()
+                            print("...........")
+                            passwd = generate_password()
+                            save_accounts(create_account(account, u_name, passwd))
+                            print('Your account has been created successfully!\n')
+                            print(f"New account: {account} \n Username: {u_name} --- Password:{passwd}")
+                            print('\n')
+
+                        elif short_code == 'da':
+                            print("-" * 10)
+                            if display_account() != []:
+                                print("\nHere is a list of all your accounts:\n")
+                                for item in display_account():
+                                    print(item.account_name + "\t --> " + item.username + "\t --> " + item.password)
+                            else:
+                                print("\n You do not have any accounts saved \n")
+
+                        elif short_code == 'sa':
+                            print("-" * 10)
+                            print("\n Enter an account type to search. eg:Facebook\n")
+                            search = input()
+                            if search_accounts(search):
+                                found = search_accounts(search)
+                                print("\n Here's what we've found:\n")
+                                print(found.account_name + "\t" + found.username + "\t" + found.password)
+
+                        elif short_code == 'd':
+                            print("-" * 10)
+                            print("Enter the account name for the record you want to delete eg: Facebook")
+                            item = input()
+                            if search_accounts(item):
+                                delete_accounts(search_accounts(item))
+                                print("-" * 10)
+                                print("The account has been deleted successfully")
+                                print("Here is a list of your remaining accounts:\n")
+                                for remaining in display_account():
+                                    print(
+                                        remaining.account_name + "\t --> " + remaining.username + "\t --> " + remaining.password)
+                            else:
+                                print("There is no record of the item you are trying to delete")
+
+                        elif short_code == "ex":
+                            print("-" * 10)
+                            print(f"Have a lovely day, {account_owner}")
+                            break
+                        else:
+                            print("-" * 10)
+                            print("\nInvalid short code. Please take your time to read them, and try again\n")
+                else:
+                    
